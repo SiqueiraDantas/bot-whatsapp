2f6f8ee4081f56a18792272cbfcfba491d950404
Siqueira Dantas <siqdantas27@gmail.com>
2026-04-28 21:52:20 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -103,6 +103,10 @@ class Handler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write(b"Bot rodando!")
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
     def log_message(self, format, *args):
         return
 
